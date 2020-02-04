c9b2a93c57b6b6d554cf23d7cdf49753cf69a358
EdecoNG-Fredrick <atimileyi@edeco.info>
2020-02-04 16:57:18 +0000

    Sec commit

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -3,6 +3,10 @@
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 
 use App\User;
+use App\Category;
+use App\Product;
+use App\Seller;
+use App\Transaction;
 use Faker\Generator as Faker;
 use Illuminate\Support\Str;
 
@@ -24,5 +28,36 @@ $factory->define(User::class, function (Faker $faker) {
         'email_verified_at' => now(),
         'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
         'remember_token' => Str::random(10),
+        'verified' => $verified = $faker->randomElement([User::VERIFIED_USER,User::UNVERIFIED_USER]),
+        'verification_token' => $verified === User::VERIFIED_USER ? null : User::generateVerificationCode(),
+        'verified' => $verified = $faker->randomElement([User::ADMIN_USER,User::REGULAR_USER]),
+    ];
+});
+
+$factory->define(Category::class, function (Faker $faker) {
+    return [
+        'name' => $faker->word,
+        'description' => $faker->paragraphs(1),
+    ];
+});
+
+$factory->define(Product::class, function (Faker $faker) {
+    return [
+        'name' => $faker->word,
+        'email' => $faker->paragraphs(1),
+        'quantity' => $faker->numberBetween(1,10),
+        'status' => $faker->randomElement([Product::AVAILABLE_PRODUCT,Product::UNAVAILABLE_PRODUCT]),
+        'image' => $faker->randomElement(['1.jpg','2.jpg','3.jpg']),
+        'seller_id' => User::all()->random()->id,
+    ];
+});
+
+$factory->define(Transaction::class, function (Faker $faker) {
+    $seller = Seller::has('products')->get()->random();
+    $buyer = User::all()->except($seller->id)->random();
+    return [
+        'quantity' => $faker->numberBetween(1,3),
+        'buyer_id' => $buyer->id,
+        'product_id' => $seller->products->random()->id,
     ];
 });
